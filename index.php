38bb1a1fe83c669f2882e1dc437073a74d311f5c
JoaoVACoelh <126514610+JoaoVACoelh@users.noreply.github.com>
2023-02-27 16:47:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Atv 1</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+</head>
+<body>
+    <h1>
+    <?php
+      echo ("ola mundo") ; 
+    ?>
+    </h1>
+
+    <h2><?php  echo ("Adição");  ?></h2>
+    <h2><?php  echo ("Subtração");  ?></h2>
+    <h2><?php  echo ("Divisão");  ?></h2>
+    <h2><?php  echo ("Multiplicação");  ?></h2>
+
+
+
+</body>
+</html>
